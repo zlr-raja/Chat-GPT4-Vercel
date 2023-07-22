d0244fc6c21f1843385c4204ef92d9f0d9f407fd
Rajashekhar <adukani@celitotech.com>
2023-07-22 13:34:54 +0530

    removed sidepanel

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,15 @@
 import Image from 'next/image'
-import ChatComponent from '@/components/ChatComponent';
- 
+import Chat from '@/components/Chat';
+
 
 export default function Home() {
 
   return (
     <div className='flex columns-3 gap-4 p-4 h-screen bg-slate-900'>
-      {/* <div className='w-1/4 h-full'>
-        <div className="card bg-slate-800 shadow-xl items-end h-full rounded-3xl">
-          <div className="card-body w-full p-4">
-            <h2 className="card-title">Resume GPT</h2>
-
-            <div className='absolute bottom-0 left-0 right-0'>
-
-            </div>
-          </div>
-        </div>
-      </div> */}
       <div className='w-full h-full'>
         <div className="card bg-slate-800 shadow-xl items-end h-full rounded-3xl">
           <div className="card-body w-full p-4">
-            <ChatComponent /> 
+            <Chat />
           </div>
         </div>
       </div>
